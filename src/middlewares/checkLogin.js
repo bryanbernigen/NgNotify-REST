e77eed73402ea3312138b88b895a7664926b5794
Kyle <kyle.treoxer@gmail.com>
2022-11-26 16:13:16 +0700

    Update middleware fix

--- src/middlewares/checkLogin.js
+++ src/middlewares/checkLogin.js
@@ -7,7 +7,10 @@ module.exports = function() {
         const token = authHeader && authHeader.split(' ')[1]
         if (token == null) return res.status(401).json({message: "No token provided"})
         jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, user) => {
-            if (err) return res.status(403).json({message: "Invalid token"})
+            if (err) {
+                res.status(403).json({message: "Invalid token"})
+                return
+            }
             req.user = user
             next()
         })
--- src/middlewares/checkParams.js
+++ src/middlewares/checkParams.js
@@ -1,13 +1,11 @@
 module.exports = function(required = []) {
     return function(req, res, next){
-        console.log(required)
         var missing_params = []
         required.forEach((param) => {
             if (req.body[param] == null) {
                 missing_params.push(param)
             }
         })
-        console.log(missing_params)
         if (missing_params.length > 0) {
             res.status(400).json({message: "Missing parameters: " + missing_params.join(", ")})
             return
